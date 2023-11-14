5804791d17fa9ace59fbf67a3329f21df0dbe0c4
anggimuammarhanafi <anggimuammarhanafi@gmail.com>
2023-11-14 14:41:30 +0700

    Push pertama saya untuk project laravel kedalam github

---
+++ resources/views/index.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Home</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/mahasiswa">Mahasiswa</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/profile">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/about">About</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
+   <main> 
+     <div class="container">
+     <center>
+     <h1 style="text-center; font-size:70px; font-weight:400; line-height:90px; font-family: DaulphinPlain;">Lorem Ipsum</h1>
+     </center>
+     <h2 style="margin: 10px 10px 5px 10px; font-size: 14px; line-height: 18px; text-align: center; font-style: italic;">
+     "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h2> 
+     <h2 style="margin: 5px 10px 20px 10px; font-size: 12px; line-height: 14px; text-align: center; font-family: Open Sans">
+     "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h2>
+     <hr>
+     <div class="row">
+        <div class="col-md-6"> 
+          <h5>What is lorem ipsum?</h5> 
+          <p style="text-align:justify"><b> Lorem Ipsum </b> is simply dummy text of the printing and typesetting industry
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+           when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+         <div class="col-md-6"> 
+          <h5>Why do we use it?</h5> 
+           <p style="text-align:justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+           The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        </div>       
+    </div>
+
+       <br><br/>
+
+    <div class="row ">
+       <div class="col-md-6"> 
+       <h5>Where does it comes from?</h5> 
+       <p style="text-align:justify">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+       </div>
+       <div class="col-md-6"> 
+       <h5>Where can i get some?</h5> 
+       <p style="text-align:justify">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+        If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+       </div>       
+    </div>
+ </main>
+ <footer class="container bg-light">
+  <div class="row">
+      <div class="col-12 py-4">
+        <p>&copy;   Nama : Anggi Muammar Hanafi<br/>
+                    Kelas: A1 TI Pagi<br/>
+                    NPM  : 2109020032</p>
+      </div>
+  </div>
+</footer>
+</body>
+</html>
---
+++ resources/views/table.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contoh Penggunaan Atribut Colspan dan Rowspan Tag Tabel</title>
+</head>
+<body>
+<h1>Contoh atribut colspan dan rowspan </h1>
+<table class = table, border="1">
+    <tr>
+       <td colspan="2" >A</td>
+       <td colspan="1" >B</td>
+    </tr>
+    <tr>
+        <td>C</td>
+        <td>D</td>
+        <td rowspan="2">E</td>
+    </tr>
+    <tr>
+        <td colspan="2"> F</td>
+    </tr>
+</table>
+</body>
+</html>
